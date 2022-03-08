490fd4538e19564f550b481fa6fd329e37c80485
Jani Palomino <palomino.jani@gmail.com>
2022-03-08 09:33:31 -0700

    Set state and destructure props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import {getOrders} from '../../apiCalls';
+import { getOrders } from '../../apiCalls';
 import Orders from '../../components/Orders/Orders';
 import OrderForm from '../../components/OrderForm/OrderForm';
 
 class App extends Component {
   constructor(props) {
     super();
+      this.state = {
+        orders: []
+      }
   }
 
   componentDidMount() {
     getOrders()
+      .then(data => {
+        this.setState({
+          orders: data.orders
+        })
+      })
       .catch(err => console.error('Error fetching:', err));
   }
 
