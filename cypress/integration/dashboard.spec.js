b8e4c04b76bbcedc03076f818f75631283373ba4
Jani Palomino <palomino.jani@gmail.com>
2022-03-08 13:35:42 -0700

    Add new order fixture and post test

--- cypress/integration/dashboard.spec.js
+++ cypress/integration/dashboard.spec.js
@@ -7,7 +7,9 @@ describe('Feedback Loop login flows', () => {
 describe('Dashboard inital page load', () => {
   beforeEach(() => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/orders', {fixture: 'orders.json'})
-    cy.visit('http://localhost:3000')
+
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {fixture: 'neworder.json'})
+    cy.visit('http://localhost:3000');
   })
   
   it('Should be able to visit the page and see a form with instructions and a submit button', () => {
@@ -19,8 +21,28 @@ describe('Dashboard inital page load', () => {
   })
 
   it('Should be able to see intial orders with the order name, and a list of ingredients in each ', () => {
-    cy.get('section > :nth-child(1) > h3')
+    cy.get('div[class="order"]').should('have.length', 3)
+    cy.get('h3')
       .get('ul').first().should('have.class', 'ingredient-list')
       .children('li').should('have.length', 5 || 6)
+  })
+
+  it('User should be able to type in their name, select at least 1 ingredient, click submit, then see their order appear on the page', () => {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/orders', {
+      statusCode: 200,
+      body: {fixture: 'neworder.json'}
+    })
+    cy.visit('http://localhost:3000');
+
+    cy.get('input').type('Jani')
+      .get('button[name="beans"]').click()
+      .get('button[name="lettuce"]').click()
+      .get('button[name="carnitas"]').click()
+      .get('button[name="queso fresco"]').click()
+      .get('button[class="submit-button"]').click()
+      .get('div[class="order"]').should('have.length', 3)
+      .get('h3').last()
+      .get('ul').first().should('have.class', 'ingredient-list')
+      .children('li').should('have.length', 5)
   })
 })
